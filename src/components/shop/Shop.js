54707aff25d132c2e17a02a3911e3ac5294920c5
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-02-13 23:13:02 +0600

    Cart Area

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    //1.Data Load Using Local File
+//----1.Data Load Using Local File
     const [products,setProducts] = useState([]);
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
-    //1.End
+//----1.End
     return (
         <div className="shop-container">
            <div className="products-container">
-                <h3>This is for products: {products.length}</h3>
+{/*--2.Dynamic Data Load*/}
+                {
+                    products.map(product=><Product 
+                                           key={product.id}
+                                           product={product}
+                                          ></Product>)
+                }
+{/*--2.End*/}
            </div>
            <div className="cart-container">
                 <h4>Order Summery</h4>
