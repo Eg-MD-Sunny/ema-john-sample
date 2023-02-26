ec79038ff4f7d129ea4dc7b3a93602fc53c134d6
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-02-26 23:06:58 +0600

    Modify

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -20,7 +20,7 @@ const Cart = (props) => {
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
-            <h3>Grand Total: {grandTotal.toFixed(3)}</h3>
+            <h3>Grand Total: {grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
